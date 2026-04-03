7a2ba2afae0f39828f4915cde1eeafb30b9e85a6
loys <mlier@free.fr>
2026-04-03 23:48:45 +0200

    Test : vérification cas ACCES

--- conso-elec-sge/test/Conso/Fr/Elec/Sge/CommanderAccesDonneesMesuresV10Spec.hs
+++ conso-elec-sge/test/Conso/Fr/Elec/Sge/CommanderAccesDonneesMesuresV10Spec.hs
@@ -42,7 +42,7 @@ shouldRefuserUnlessActiveHomo prm duree typeDonnees expectedCode = pendingOnNetw
 shouldRefuserSansAccordHomo :: String -> String -> Expectation
 shouldRefuserSansAccordHomo prm expectedCode = pendingOnNetworkError $ do
     cleanupServices prm
-    myType <- initTypeTest prm (Just (3 * 365)) Nothing "CDC" SensSOUTIRAGE
+    myType <- initTypeTest prm (Just (3 * 365)) Nothing "ENERGIE" SensSOUTIRAGE
     rep    <- wsRequestTest myType :: IO (Either (String, String) CommanderAccesDonneesMesuresResponseType)
     rep `shouldHaveCode` expectedCode
 
@@ -51,17 +51,26 @@ spec :: Spec
 spec = do
     describe homologationC $ do
         describe recevablesC $ do
-            it "ACCES-R1 - Accès CDC C5 sur 3 ans" $ do
+            it "ACCES-R1 C5 - Commande d’accès aux données d’ENERGIES globales quotidiennes C5 sur 3 ans" $ do
+                shouldCommanderHomo accesPrmC5 (Just (3 * 365)) "ENERGIE"
+
+            it "ACCES-R1 C2C4 - Commande d’accès aux données d’ENERGIES globales quotidiennes C2-C4 sur 3 ans" $ do
+                shouldCommanderHomo accesPrmC2C4 (Just (3 * 365)) "ENERGIE"
+
+            it "ACCES-R2 C5 - Commande d’accès aux données de CDC C5 sur 3 ans" $ do
                 shouldCommanderHomo accesPrmC5 (Just (3 * 365)) "CDC"
 
-            it "ACCES-R2 - Accès IDX C5 sur 3 ans" $ do
-                shouldCommanderHomo accesPrmC5 (Just (3 * 365)) "IDX"
-
-            it "ACCES-R3 - Accès CDC C2-C4 sur 3 ans" $ do
+            it "ACCES-R2 C2C4 - Commande d’accès aux données de CDC C2-C4 sur 3 ans" $ do
                 shouldCommanderHomo accesPrmC2C4 (Just (3 * 365)) "CDC"
 
-            it "ACCES-R4 - Accès ENERGIE C5 avec durée 3 ans" $ do
-                shouldCommanderHomo accesPrmC5 (Just (3 * 365)) "ENERGIE"
+            it "ACCES-R3 C5 - Commande d’accès aux données de PMAX C5 sur 3 ans" $ do
+                shouldCommanderHomo accesPrmC5 (Just (3 * 365)) "PMAX"
+
+            it "ACCES-R4 C5 - Commande d’accès aux données d’INDEX C5 avec durée 3 ans" $ do
+                shouldCommanderHomo accesPrmC5 (Just (3 * 365)) "IDX"
+
+            it "ACCES-R4 C2C4 - Commande d’accès aux données d’INDEX C2-C4 avec durée 3 ans" $ do
+                shouldCommanderHomo accesPrmC2C4 (Just (3 * 365)) "IDX"
 
         describe nonRecevablesC $ do
             it "ACCES-NR1 C5 - Sans accord client (SGT566)" $ do
@@ -71,10 +80,10 @@ spec = do
 
             it "ACCES-NR2 C5 - Durée supérieure à 3 ans (SGT5O9)" $ do
                 -- La durée dépasse la limite autorisée de 3 ans (1096 jours).
-                shouldRefuserUnlessActiveHomo accesPrmC5 (Just 1500) "CDC" "SGT5O9"
+                shouldRefuserUnlessActiveHomo accesPrmC5 (Just 1500) "ENERGIE" "SGT5O9"
             it "ACCES-NR2 C2C4 - Durée supérieure à 3 ans (SGT5O9)" $ do
                 -- La durée dépasse la limite autorisée de 3 ans (1096 jours).
-                shouldRefuserUnlessActiveHomo accesPrmC2C4 (Just 1500) "CDC" "SGT5O9"
+                shouldRefuserUnlessActiveHomo accesPrmC2C4 (Just 1500) "ENERGIE" "SGT5O9"
 
 
 main :: IO ()
